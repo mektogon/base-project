071a881a19f21eb1923576ac523faf303a0bebcf
Maxim Dorofeev <Mektogon385@mail.ru>
2024-03-01 21:53:23 +0700

    BP - Добавление базовых таблиц.

---
+++ src/main/resources/db/changelog/01.000.00/1709304025339_create_task_table.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+--changeset Maxim Dorofeev:1709304025339_create_task_table.sql
+
+CREATE TABLE job
+(
+    name        VARCHAR(100) PRIMARY KEY NOT NULL,
+    description VARCHAR(500),
+    enabled     BOOLEAN                  NOT NULL DEFAULT TRUE,
+    create_date TIMESTAMP                NOT NULL DEFAULT NOW(),
+    update_date TIMESTAMP                NOT NULL DEFAULT NOW(),
+    version     BIGINT                   NOT NULL DEFAULT 0
+);
+
+-- ROLLBACK DROP TABLE job;
+
+COMMENT ON TABLE job IS 'Таблица с запускаемыми задачами';
+
+COMMENT ON COLUMN job.name IS 'Уникальное наименование задачи';
+COMMENT ON COLUMN job.description IS 'Описание задачи';
+COMMENT ON COLUMN job.enabled IS 'Включена ли задача (true\false)';
+COMMENT ON COLUMN job.create_date IS 'Дата создания записи';
+COMMENT ON COLUMN job.update_date IS 'Дата последнего обновления записи';
+COMMENT ON COLUMN job.version IS 'Номер версии объекта';
