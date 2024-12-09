b8cc77b9a4620c367d18fc0103a7c9a71462fdc4
Maxim Dorofeev <Mektogon385@mail.ru>
2024-12-10 01:53:52 +0700

    BP - Настройка задачи блокировки версий зависимостей; Правка srcDir для github actions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,19 @@ allprojects {
     }
 
     dependencyLocking {
-        lockFile.set(file("$projectDir/gradle/lockfiles/${rootProject.name}-${version}.lockfile"))
+        lockFile.set(file("$rootDir/gradle/lockfiles/${project.name}-${version}.lockfile"))
         lockAllConfigurations()
+    }
+
+    tasks.register("resolveAndLockAll") {
+        notCompatibleWithConfigurationCache("Filters configurations at execution time")
+        doFirst {
+            require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
+        }
+        doLast {
+            configurations.filter {
+                it.isCanBeResolved
+            }.forEach { it.resolve() }
+        }
     }
 }
