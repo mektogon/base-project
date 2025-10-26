f1cce6f671385b1b0efcf7fec2d757ff31fe3c13
Maxim Dorofeev <Mektogon385@mail.ru>
2025-10-26 18:39:03 +0700

    BP - Реконфигурация структуры проекта. - Вынесение основного приложения в отдельный модуль: "business-application" - Настройка build.gradle.kts согласно новым модулям.

---
+++ back/business-application/build.gradle.kts
@@ -0,0 +1,43 @@
+dependencies {
+    implementation(project(":back:database"))
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.13.5")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+    testImplementation("io.rest-assured:rest-assured:5.5.5")
+    //https://allurereport.org
+    implementation("io.qameta.allure:allure-rest-assured:2.29.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+}
+
+sourceSets {
+    main {
+        resources {
+            srcDirs("${rootDir}/back/database/src/main/resources")
+        }
+    }
+    test {
+        resources {
+            srcDirs("${rootDir}/back/database/src/test/resources")
+        }
+    }
+}
+
+tasks {
+    jar {
+        enabled = false //Disable generation plain-file for jar-package
+    }
+
+    bootRun {
+        if (project.hasProperty("args")) { //Many arguments for bootRun
+            args(project.properties["args"]?.toString()?.split(",") as Iterable<String>)
+        }
+    }
+}
